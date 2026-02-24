0e478b3bc2b4f5cea9eef4c18cab78bea8f574fb
From.deuk <from.deuk@gmail.com>
2026-02-24 17:17:34 +0900

    fg

--- src/components/bottomSheet/BottomSheet.tsx
+++ src/components/bottomSheet/BottomSheet.tsx
@@ -860,7 +860,7 @@ const BottomSheetComponent = forwardRef<BottomSheet, BottomSheetProps>(
           isInTemporaryPosition.value = true;
           const keyboardHeightInContainer =
             animatedKeyboardState.get().heightWithinContainer;
-          return Math.max(0, highestDetentPosition - keyboardHeightInContainer - 50);
+          return Math.max(0, highestDetentPosition - keyboardHeightInContainer - 48);
         }
 
         /**
